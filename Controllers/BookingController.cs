73b91bd82ab2789d04b706707a95751920b7aff4
SvetlanaPetrovic262 <svetlanasovljanski@gmail.com>
2024-11-19 19:40:50 +0100

    Add project files.

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentalCars.Models;
+
+namespace RentalCars.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Booking(int CarId)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Booking(Booking booking)
+        {
+            return View();
+        }
+        public IActionResult Confirmation(int bookingId)
+        {
+            return View();
+        }
+        public IActionResult Return()
+        {
+            return View();
+        }
+        public IActionResult History(int userId)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace RentalCars.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public int CardId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
